f08d8cce6a780003ddad7822929c30b79acf0c54
kyle <kyle@Craptop>
2014-09-23 19:30:20 -0400

    Updated the drive to optional single stick. Changed the calibrate controls to be on button 12.

--- src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -18,22 +18,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class RobotMain extends IterativeRobot
 {
+
     //sets if cam is to ever be intialized and used
+
     private final boolean CAMERA_ENABLED = false;
-    
+
+    public static final boolean SINGLE_STICK_DRIVE = true;
+
     //Robot objects
     private Driverstation driverstation;
     private Drive drive;
-    
+
     private static Feeder feeder;
     //private Camera467 cam;
     private Launcher launcher;
     private boolean enabledOnce = false;
-    
+
     private Compressor467 comp;
     private GearToothSensor gts;
     //private LEDring LED;
-    
+
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -49,10 +53,9 @@ public class RobotMain extends IterativeRobot
         launcher = Launcher.getInstance();
         //cam = Camera467.getInstance();
         gts = new GearToothSensor(4, RobotMap.WHEEL_CIRCUMFRENCE, RobotMap.TICKS_PER_WHEEL);
-        
+
         //SpeedCalibration.init();
         //LED = LEDring.getInstance();
-
         // static static static static static
         Calibration.init();
     }
@@ -94,80 +97,80 @@ public class RobotMain extends IterativeRobot
      */
     public void testInit()
     {
-        
-         gts.reset();
-         gts.start();
-        
-         //startTime = System.currentTimeMillis();
+
+        gts.reset();
+        gts.start();
+
+        //startTime = System.currentTimeMillis();
     }
+
     /**
      * This function is called periodically test mode
      */
     public void testPeriodic()
     {
         /*
-        driverstation.readInputs();
-        Joystick467 joy = driverstation.getNavJoystick();
-//        if(joy.buttonDown(Joystick467.TRIGGER))
-//        {            
-//            drive.driveFeeder(joy.getStickY());
-//        }
+         driverstation.readInputs();
+         Joystick467 joy = driverstation.getNavJoystick();
+         //        if(joy.buttonDown(Joystick467.TRIGGER))
+         //        {            
+         //            drive.driveFeeder(joy.getStickY());
+         //        }
         
-        comp.update();
-        */
-        
+         comp.update();
+         */
+
         gts.update();
         System.out.println("Ticks: " + gts.getTicks());
-        
+
         //<editor-fold defaultstate="collapsed" desc="Commented Out Test Periodic Code">
         /*
-        driverstation.readInputs();
-        driverstation.clearPrint();
-        Joystick467 joy = driverstation.getRightJoystick();
+         driverstation.readInputs();
+         driverstation.clearPrint();
+         Joystick467 joy = driverstation.getRightJoystick();
         
-        double speed = joy.getStickY();
+         double speed = joy.getStickY();
         
-        if (joy.buttonDown(4))
-        {
-        drive.driveParasite(speed);
-        }
+         if (joy.buttonDown(4))
+         {
+         drive.driveParasite(speed);
+         }
         
-        if (joy.buttonDown(9))
-        {
-        gts.reset();
-        }
+         if (joy.buttonDown(9))
+         {
+         gts.reset();
+         }
         
-        gts.update();
+         gts.update();
         
-        double RPM = gts.getAccurateRPM();
+         double RPM = gts.getAccurateRPM();
         
-        driverstation.println("Parasite Wheel", 1);
-        driverstation.println("RPM: " + (int) RPM, 2);
-        driverstation.println("Ticks: " + gts.getTicks(), 3);
-        driverstation.println("Power: " + drive.getParasite().get(), 4);
-        driverstation.println("Speed: ~" + (int) gts.convertRPMtoVelocity(RPM) + " ft/s", 5);
+         driverstation.println("Parasite Wheel", 1);
+         driverstation.println("RPM: " + (int) RPM, 2);
+         driverstation.println("Ticks: " + gts.getTicks(), 3);
+         driverstation.println("Power: " + drive.getParasite().get(), 4);
+         driverstation.println("Speed: ~" + (int) gts.convertRPMtoVelocity(RPM) + " ft/s", 5);
         
-        if (joy.buttonDown(5))
-        {
-        drive.driveParasite(Math.sin((System.currentTimeMillis() - startTime)*.01));
-        }
+         if (joy.buttonDown(5))
+         {
+         drive.driveParasite(Math.sin((System.currentTimeMillis() - startTime)*.01));
+         }
         
-        driverstation.sendData();
-        */
-        
+         driverstation.sendData();
+         */
         /*
-        Steering steering = drive.getSteering(RobotMap.FRONT_LEFT);
+         Steering steering = drive.getSteering(RobotMap.FRONT_LEFT);
         
-        steering.getMotor().set(.3);
+         steering.getMotor().set(.3);
         
-        double val = drive.getSteering(RobotMap.FRONT_LEFT).getSensorValue();
+         double val = drive.getSteering(RobotMap.FRONT_LEFT).getSensorValue();
         
-        if (val > steeringRange) {
-        steeringRange = val;
-        }
+         if (val > steeringRange) {
+         steeringRange = val;
+         }
         
-        System.out.println(steeringRange);
-        */
+         System.out.println(steeringRange);
+         */
 //</editor-fold>
     }
 
@@ -187,32 +190,39 @@ public class RobotMain extends IterativeRobot
         //Read driverstation inputs
         driverstation.readInputs();
         driverstation.clearPrint();
-        comp.update();                
+        comp.update();
 
         //
         //Branch based on mode
         //Use driver's stick
-        if (driverstation.getDriveJoystick().getFlap())
+        if (driverstation.getDriveJoystick().buttonPressed(12))
         {
             driverstation.println("Mode: Calibrate", 1);
             updateCalibrateControl();
         }
         else
         {
-            driverstation.println("Mode: Drive", 1);
-            updateDriveControl();
-            updateNavigatorControl();
+            if (SINGLE_STICK_DRIVE)
+            {
+                updateSingleStickControl();
+            }
+            else
+            {
+                driverstation.println("Mode: Drive", 1);
+                updateDriveControl();
+                updateNavigatorControl();
+            }
 //            System.out.println("FL: " + drive.getSteering(RobotMap.FRONT_LEFT).getSensorValue());
         }
 
         //Send printed data to driverstation
         driverstation.sendData();
     }
-
+    
     /**
-     * Update normal driver control
+     * Update the control with a single stick
      */
-    private void updateDriveControl()
+    private void updateSingleStickControl()
     {
         //Speed to drive at (negative speeds drive backwards)
         double speed;
@@ -228,7 +238,7 @@ public class RobotMain extends IterativeRobot
         {
             // Speed for car drive
             speed = joyLeft.getStickY();
-        }        
+        }
         else//CRAB OR FIELD ALIGNED
         {
             // Speed for crab drive, field aligned or otherwise.
@@ -261,20 +271,126 @@ public class RobotMain extends IterativeRobot
         {
             // Drive field aligned if button 5 is pressed
             drive.crabDrive(joyLeft.getStickAngle(), speed, true /*field aligned*/);
-        }        
+        }
         else if (joyLeft.buttonDown(3))//CAR DRIVE
         {
             //Car drive if button 3 is pressed.
             // Stick twist controls turning, and stick Y controls speed.
             drive.carDrive(joyLeft.getTwist(), speed);
-        }        
+        }
+        else//CRAB DRIVE
+        {
+            //Normally use crab drive
+            drive.crabDrive(joyLeft.getStickAngle(), speed, false/*not field aligned*/);
+        }
+        
+        //FIRE
+        if (joyLeft.getFlap())
+        {
+            launcher.fireLauncher();
+            System.out.println("Fire launcher");
+        }
+        else
+        {
+            launcher.pullBackLauncher();
+            System.out.println("Pull back launcher");
+        }
+
+        //hat stick is backward
+        if (joyLeft.getHatY() < -0.5)
+        {
+            //intake ball
+            feeder.driveFeederMotor(-1.0);
+        }
+        //hat stick is forward
+        else if (joyLeft.getHatY() > 0.5)
+        {
+            //spit out ball
+            feeder.driveFeederMotor(1.0);
+        }
+        else
+        {
+            //dont feed
+            feeder.driveFeederMotor(0);
+        }
+
+        //sets arms down or up
+        if (joyLeft.getStickY() < -0.5)
+        {
+            feeder.lowerFeeder();
+        }
+        else
+        {
+            feeder.raiseFeeder();
+        }
+    }
+
+    /**
+     * Update normal driver control
+     */
+    private void updateDriveControl()
+    {
+        //Speed to drive at (negative speeds drive backwards)
+        double speed;
+        Joystick467 joyLeft = driverstation.getDriveJoystick();
+
+        //Set speed
+        if (joyLeft.buttonDown(2))//TURN IN PLACE
+        {
+            // Speed for turn in place
+            speed = joyLeft.getTwist();
+        }
+        else if (joyLeft.buttonDown(3) || joyLeft.buttonDown(6))//CAR DRIVE
+        {
+            // Speed for car drive
+            speed = joyLeft.getStickY();
+        }
+        else//CRAB OR FIELD ALIGNED
+        {
+            // Speed for crab drive, field aligned or otherwise.
+            speed = joyLeft.getStickDistance();
+        }
+
+        // Speed modifiers
+        if (joyLeft.buttonDown(Joystick467.TRIGGER))//CREEP
+        {
+            // Creep on trigger
+            speed /= 3.0;
+        }
+        else if (joyLeft.buttonDown(7))//TURBO
+        {
+            // Turbo on button 7
+            speed *= 2.0;
+        }
+
+        SmartDashboard.putNumber("Speed", speed);
+        //SmartDashboard.putNumber("Current Angle", gyro.getAngle());
+        SmartDashboard.putNumber("Battery Usage", driverstation.getBatteryVoltage());
+
+        //Decide drive mode
+        if (joyLeft.buttonDown(2))//TURN DRIVE
+        {
+            //Rotate in place if button 2 is pressed
+            drive.turnDrive(-speed);
+        }
+        else if (joyLeft.buttonDown(5))//FIELD ALIGNED
+        {
+            // Drive field aligned if button 5 is pressed
+            drive.crabDrive(joyLeft.getStickAngle(), speed, true /*field aligned*/);
+        }
+        else if (joyLeft.buttonDown(3))//CAR DRIVE
+        {
+            //Car drive if button 3 is pressed.
+            // Stick twist controls turning, and stick Y controls speed.
+            drive.carDrive(joyLeft.getTwist(), speed);
+        }
         else//CRAB DRIVE
         {
             //Normally use crab drive
             drive.crabDrive(joyLeft.getStickAngle(), speed, false/*not field aligned*/);
         }
 
-        if ( CAMERA_ENABLED && joyLeft.buttonPressed(11))//TOGGLE CAMERA
+        if (CAMERA_ENABLED && joyLeft.buttonPressed(11))//TOGGLE CAMERA
         {
             // Toggle camera if button 11 is pressed
             //cam.toggleReading();
@@ -296,20 +412,21 @@ public class RobotMain extends IterativeRobot
         {
             // Print camera status to driver station
             /*
-            driverstation.println((cam.isReading())
-                    ? "Target detected: " + ((cam.isTargetDetected()) ? "Yes" : "No")
-                    : "Camera is not reading.", 4);
-            */
+             driverstation.println((cam.isReading())
+             ? "Target detected: " + ((cam.isTargetDetected()) ? "Yes" : "No")
+             : "Camera is not reading.", 4);
+             */
         }
     }
 
     //this is a persistant variable to select the wheel to calibrate.
     int motorId = 0;
+
     /**
      * Update steering calibration control
      */
     private void updateCalibrateControl()
-    {        
+    {
         double stickAngle = driverstation.getDriveJoystick().getStickAngle();
 
         //Branch into motor being calibrated
@@ -355,19 +472,19 @@ public class RobotMain extends IterativeRobot
     private void updateNavigatorControl()
     {
         Joystick467 joyNav = driverstation.getNavJoystick();
-                
+
         //FIRE
         if (joyNav.getFlap())
         {
             launcher.fireLauncher();
             System.out.println("Fire launcher");
-        }        
+        }
         else
         {
             launcher.pullBackLauncher();
             System.out.println("Pull back launcher");
         }
-        
+
         //hat stick is backward
         if (joyNav.getHatY() < -0.5)
         {
@@ -383,9 +500,9 @@ public class RobotMain extends IterativeRobot
         else
         {
             //dont feed
-            feeder.driveFeederMotor(0);            
+            feeder.driveFeederMotor(0);
         }
-        
+
         //sets arms down or up
         if (joyNav.getStickY() < -0.5)
         {
